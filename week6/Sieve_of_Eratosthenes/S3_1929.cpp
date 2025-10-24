961e90a90c5ddcd504d5dfca0fa934b3ec553926
Hong0206 <hongyd20042@gmail.com>
2025-10-24 21:30:43 +0900

    BOJ(1929) 소수 구하기
    에라토스테네스의 체 알고리즘을 사용하여, 소수를 판별하는 문제
    
    범위는 2부터 원하는 값의 제곱근 까지 반복하며,
    반복문 내에는 소수인 경우 그 값은 건너뛰고, 그 값의 배수는
    모두 소수가 아닌것으로 처리함
    
    O(N log(log N))

---
+++ week6/Sieve_of_Eratosthenes/S3_1929.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// 에라토스테네스의 체 알고리즘을 사용하여
+// index 에 대한 값이 소수인지 아닌지를 판별하여,
+// bool 값으로 저장
+void sieve_of_eratosthenes(vector<bool> &v) {
+    // 0은 소수 판별에 사용하지 않음
+    v[0] = false;
+
+    // 1은 소수 판별에 사용하지 않음
+    v[1] = false;
+
+    // 2부터 시작함 ~ 원하는 범위의 제곱근까지
+    for (int i = 2; i < sqrt(v.size()); i++) {
+        // 현재의 벡터 값이 true(소수) 인 경우
+        if (v[i]) {
+            int idx = i;
+            // 해당 값은 넘어가고,
+            // 그 값의 배수들은 소수가 아니므로, false로 값 갱신
+            for (int j = i * 2; j < v.size(); j += i) v[j] = false;
+        }
+    }
+}
+
+int main(void) {
+
+    // 입출력 고속화
+    ios::sync_with_stdio(false), cin.tie(NULL);
+
+    // 1~1,000,000 까지
+    vector<bool> v(1000001, true);
+
+    // 함수 호출해서 소수 판별
+    sieve_of_eratosthenes(v);
+
+    // M~N 까지의 범위를 나타내는 입력값 받기
+    int M, N;
+    cin >> M >> N;
+
+    // 답안 출력
+    for (int i = M; i <= N; i++)
+        if (v[i] == true) cout << i << '\n';
+
+    return 0;
+}
